4d48ff49b9ee8662b1040448862b3e43b1892ca1
Austin Johnson <atjohns2@yahoo.com>
2017-12-10 21:52:04 -0500

    Add rarity backgrounds

--- src/components/ResultsGrid.js
+++ src/components/ResultsGrid.js
@@ -5,6 +5,9 @@ import './App.css';
 import ThreeStar from '../images/3star.png';
 import FourStar from '../images/4star.png';
 import FiveStar from '../images/5star.png';
+import ThreeStarBg from '../images/3bg.png';
+import FourStarBg from '../images/4bg.png';
+import FiveStarBg from '../images/5bg.png';
 import hrt from '../images/hrt.png';
 import bod from '../images/bod.png';
 import brv from '../images/brv.png';
@@ -20,9 +23,21 @@ const ResultsGrid = (props) => (
             {row.map((col, index) =>
                 <td key={index}>                   
                     <div className={col.thumb + " " + (col.type.includes('b-featured') ? 'featured' : '')} />
-                    {col.rarity === '5' ? (<img src={FiveStar} className="rarity" alt="rarity"/>) : null } 
-                    {col.rarity === '4' ? (<img src={FourStar} className="rarity" alt="rarity"/>) : null } 
-                    {col.rarity === '3' ? (<img src={ThreeStar} className="rarity" alt="rarity"/>) : null } 
+                    {col.rarity === '5' ? (
+                    <div>
+                        <img src={FiveStar} className="rarity" alt="rarity"/>
+                        <img src={FiveStarBg} className="raritybg" alt="raritybg"/>
+                    </div>) : null } 
+                    {col.rarity === '4' ? (
+                    <div>
+                        <img src={FourStar} className="rarity" alt="rarity"/>
+                        <img src={FourStarBg} className="raritybg" alt="raritybg"/>
+                    </div>) : null } 
+                    {col.rarity === '3' ? (
+                    <div>
+                        <img src={ThreeStar} className="rarity" alt="rarity"/>
+                        <img src={ThreeStarBg} className="raritybg" alt="raritybg"/>
+                    </div>) : null } 
                     {col.cardType === 'hrt' ? (<img src={hrt} className="cardType" alt="cardType"/>) : null } 
                     {col.cardType === 'bod' ? (<img src={bod} className="cardType" alt="cardType"/>) : null } 
                     {col.cardType === 'brv' ? (<img src={brv} className="cardType" alt="cardType"/>) : null } 
