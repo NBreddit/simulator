a6c9b23ee3805d13b019483e55dadeb14c5ed579
Austin Johnson <atjohns2@yahoo.com>
2017-12-10 21:43:20 -0500

    Initial implementation of card backgrounds based on type

--- src/components/ResultsGrid.js
+++ src/components/ResultsGrid.js
@@ -5,6 +5,11 @@ import './App.css';
 import ThreeStar from '../images/3star.png';
 import FourStar from '../images/4star.png';
 import FiveStar from '../images/5star.png';
+import hrt from '../images/hrt.png';
+import bod from '../images/bod.png';
+import brv from '../images/brv.png';
+import skl from '../images/skl.png';
+import wis from '../images/wis.png';
 
 const ResultsGrid = (props) => (  
     
@@ -18,6 +23,11 @@ const ResultsGrid = (props) => (
                     {col.rarity === '5' ? (<img src={FiveStar} className="rarity" alt="rarity"/>) : null } 
                     {col.rarity === '4' ? (<img src={FourStar} className="rarity" alt="rarity"/>) : null } 
                     {col.rarity === '3' ? (<img src={ThreeStar} className="rarity" alt="rarity"/>) : null } 
+                    {col.cardType === 'hrt' ? (<img src={hrt} className="cardType" alt="cardType"/>) : null } 
+                    {col.cardType === 'bod' ? (<img src={bod} className="cardType" alt="cardType"/>) : null } 
+                    {col.cardType === 'brv' ? (<img src={brv} className="cardType" alt="cardType"/>) : null } 
+                    {col.cardType === 'skl' ? (<img src={skl} className="cardType" alt="cardType"/>) : null } 
+                    {col.cardType === 'wis' ? (<img src={wis} className="cardType" alt="cardType"/>) : null } 
                     {props.type ==='all' ? ( <span className="counter">x{col.count}</span> ): null}
                 </td>
             )}
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,8 +1,14 @@
+/*jshint loopfunc: true */
+
 import axios from 'axios';
 
+function getCallPrefix() {
+    return '/Simulator/configuration/';
+    //return '/configuration/';
+}
+
 function getAvailableBanners() {
-    var request = '/Simulator/configuration/banners.json';
-    //request = '/configuration/banners.json';
+    var request = getCallPrefix() + 'banners.json';
     var instance = axios.create({
         headers: {"accept": "application/json;odata=verbose",
             "content-type": "application/json;odata=verbose"}
@@ -20,57 +26,70 @@ function getAvailableBanners() {
 
 function getBannerRates(bannerId) {
     var summonValue = 0, summonRates = [];
-    var request = '/Simulator/configuration/banners/' + bannerId + '.json';
-    //request = '/configuration/banners/' + bannerId + '.json';
+    var cardTypeRequest = getCallPrefix() + 'type.json';
+    var request = getCallPrefix() + 'banners/' + bannerId + '.json';
 
     var instance = axios.create({
         headers: {"accept": "application/json;odata=verbose",
             "content-type": "application/json;odata=verbose"}
     });
 
-    return instance.get(request)
+    return instance.get(cardTypeRequest)
         .then(function(response) {
-            Object.keys(response.data).map(function(keyName, keyIndex) {
-                var rate = response.data[keyName].rate;
-                var unitCount = response.data[keyName].units.length;
-                var ratePerUnit = rate/unitCount;
+            var cardTypes = response.data;
+            instance.get(request)
+                .then(function(response) {
+                    Object.keys(response.data).map(function(keyName, keyIndex) {
+                        var rate = response.data[keyName].rate;
+                        var unitCount = response.data[keyName].units.length;
+                        var ratePerUnit = rate/unitCount;
 
-                var rarity = keyName.toString().split('_')[1];
-                var sort = 1;
+                        var rarity = keyName.toString().split('_')[1];
+                        var sort = 1;
 
-                switch (rarity) {
-                    case '5':
-                        sort = 1;
-                        break;
-                    case '4':
-                        sort = 2;
-                        break;
-                    case '3':
-                        sort = 3;
-                        break;
-                    default:
-                        sort = 4;
-                }
+                        switch (rarity) {
+                            case '5':
+                                sort = 1;
+                                break;
+                            case '4':
+                                sort = 2;
+                                break;
+                            case '3':
+                                sort = 3;
+                                break;
+                            default:
+                                sort = 4;
+                        }
 
-                var type = 'z-normal';
-                if (keyName.toString().split('_')[0] === 'featured') {
-                    type = 'b-featured';
-                }
+                        var type = 'z-normal';
+                        if (keyName.toString().split('_')[0] === 'featured') {
+                            type = 'b-featured';
+                        }
 
-                for (var i = 0; i < unitCount; i++) {
-                    summonValue = ratePerUnit + summonValue;
+                        for (var i = 0; i < unitCount; i++) {
+                            summonValue = ratePerUnit + summonValue;
+                            var cardType = "";
+                            
+                            Object.keys(cardTypes).map(function(typeName, typeIndex) {
+                                if(cardTypes[typeName].units.includes(response.data[keyName].units[i]))
+                                {
+                                    cardType = typeName;
+                                }
+                            });
 
-                    summonRates.push({
-                        thumb: "flair flair-" + response.data[keyName].units[i],
-                        value: summonValue,
-                        rarity: rarity,
-                        sort: sort,
-                        rate: ratePerUnit,
-                        type: type,
-                        count: 0
+                            summonRates.push({
+                                thumb: "flair flair-" + response.data[keyName].units[i],
+                                cardType: cardType,
+                                value: summonValue,
+                                rarity: rarity,
+                                sort: sort,
+                                rate: ratePerUnit,
+                                type: type,
+                                count: 0
+                            });
+                        }
                     });
-                }
-            });
+                });
             return summonRates;
         })
         .catch(function(error) {
