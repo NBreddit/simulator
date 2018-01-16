7741f8c51ac8b3f7670b094111ce7a21593c1ece
antonlabz <20198370+antonlabz@users.noreply.github.com>
2018-01-16 23:30:35 +1100

    revert test

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -81,7 +81,7 @@ function getBannerRates(bannerId) {
                             });
 
                             summonRates.push({
-                                thumb: '/simulator/src/flairs/' + response.data[keyName].units[i] + ".png",
+                                thumb: getCallPrefix() + '/flairs/' + response.data[keyName].units[i] + ".png",
                                 unitType: unitType,
                                 value: summonValue,
                                 rarity: rarity,
